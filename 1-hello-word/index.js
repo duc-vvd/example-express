d7152d615374ef16c58c3ca20c115a32c09db7a0
VuVietDuc <vuvietduc9x@gmail.com>
2020-01-06 16:24:13 +0700

    Create 'User list' link

--- 1-hello-word/index.js
+++ 1-hello-word/index.js
@@ -4,7 +4,7 @@ var app = express();
 var port = 3000;
 
 app.get('/', function (request,response) {
-	response.send('<h1>Hello word</h1>');
+	response.send('<h1>Hello word</h1><a href="/users">User list</a>');
 })
 
 app.get('/users', function (request,response) {
