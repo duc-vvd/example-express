54a38d237e7bfaf9dd1824d27769046173876f75
VuVietDuc <vuvietduc9x@gmail.com>
2020-01-06 15:41:21 +0700

    Hello word

---
+++ 1-hello-word/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+var port = 3000;
+
+app.get('/', function (request,response) {
+	response.send('<h1>Hello word</h1>');
+})
+
+app.get('/users', function (request,response) {
+	response.send('User list');
+})
+
+app.listen(port, function () {
+	console.log('Server linsting on  port ' + port);
+})
