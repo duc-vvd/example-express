456b0848824cdfb1b2b776ec1315689f124ed5ea
VuVietDuc <vuvietduc9x@gmail.com>
2020-01-08 00:21:26 +0700

    Install lowdb, Database integration

--- 1-hello-word/index.js
+++ 1-hello-word/index.js
@@ -4,13 +4,18 @@ var app = express();
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+
+var adapter = new FileSync('db.json');
+var db = low(adapter);
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ users: [] })
+  .write();
+
 var port = 3000;
 
-var users = [
-		{id: 1, name: 'Đức'},
-		{id: 2, name: 'Thúy'},
-		{id: 3, name: 'Quyết'}
-		];
 
 app.set('view engine', 'pug');
 app.set('views', './views')
@@ -23,14 +28,15 @@ app.get('/', function (request,response) {
 
 app.get('/users', function (request,response) {
 	response.render('users/index', {
-		users: users
+		users: db.get('users').value()
 	});
 })
 
 app.get('/users/search',function (req,res) {
 	var q = req.query.q;
-	var matchedUsers = users.filter(function (user) {
-			return user.name.toUpperCase().indexOf(q.toUpperCase()) !== -1;
+	var matchedUsers = db.get('users').value().filter(function (item) {
+
+			return item.name.toUpperCase().indexOf(q.toUpperCase()) !== -1;
 	});
 
 	res.render('users/index', {
@@ -43,7 +49,7 @@ app.get('/users/create', function (req,res) {
 })
 
 app.post('/users/create', function (req,res) {
-	users.push(req.body);
+	db.get('users').push(req.body).write();
 	res.redirect('/users');
 })
 
