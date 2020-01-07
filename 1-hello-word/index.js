07d7c5681ed2ff7461c788165a12e2ae47cf869b
VuVietDuc <vuvietduc9x@gmail.com>
2020-01-07 10:18:33 +0700

    Query parameters

--- 1-hello-word/index.js
+++ 1-hello-word/index.js
@@ -3,6 +3,12 @@ var app = express();
 
 var port = 3000;
 
+var users = [
+		{id: 1, name: 'Đức'},
+		{id: 2, name: 'Thúy'},
+		{id: 3, name: 'Quyết'}
+		];
+
 app.set('view engine', 'pug');
 app.set('views', './views')
 
@@ -14,11 +20,19 @@ app.get('/', function (request,response) {
 
 app.get('/users', function (request,response) {
 	response.render('users/index', {
-		users: [
-		{id: 1, name: 'Đức'},
-		{id: 2, name: 'Thúy'}
-		]
+		users: users
 	});
+})
+
+app.get('/users/search',function (req,res) {
+	var q = req.query.q;
+	var matchedUsers = users.filter(function (user) {
+			return user.name.toUpperCase().indexOf(q.toUpperCase()) !== -1;
+	});
+
+	res.render('users/index', {
+		users: matchedUsers
+	})
 })
 
 app.listen(port, function () {
