67173ec6b27de56fe1b3d39035fd626f590a170e
VuVietDuc <vuvietduc9x@gmail.com>
2020-01-09 01:10:20 +0700

    Static file

--- 1-hello-word/index.js
+++ 1-hello-word/index.js
@@ -8,16 +8,18 @@ var port = 3000;
 
 var app = express();
 app.set('view engine', 'pug');
-app.set('views', './views')
+app.set('views', './views');
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use(express.static('public'));
 
 app.get('/', function (request,response) {
 	response.render('index', {
 		name: "Đức"
 	});
-})
+});
 
 app.use('/users',userRoute);
 
