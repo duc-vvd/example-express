4cbc8d8be2810c224e4d84c971d3f15cb5bbb998
VuVietDuc <vuvietduc9x@gmail.com>
2020-01-14 23:12:31 +0700

    Pagination

--- 1-hello-word/index.js
+++ 1-hello-word/index.js
@@ -7,6 +7,7 @@ var db = require('./db');
 
 var userRoute = require('./routes/user.route');
 var authRoute = require('./routes/auth.route');
+var productRoute = require('./routes/product.route');
 
 var authMiddleware = require('./middlewares/auth.middleware');
 
@@ -30,6 +31,7 @@ app.get('/', function (request,response) {
 
 app.use('/users',authMiddleware.requireAuth, userRoute);
 app.use('/auth',authRoute);
+app.use('/products',authMiddleware.requireAuth, productRoute);
 
 app.listen(port, function () {
 	console.log('Server linsting on  port ' + port);
