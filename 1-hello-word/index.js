4686c456a3def537dcf389c92ce1ef40cec4c0f7
VuVietDuc <vuvietduc9x@gmail.com>
2020-01-14 15:22:55 +0700

    Envaironment vairiables

--- 1-hello-word/index.js
+++ 1-hello-word/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 var express = require('express');
 var cookieParser = require('cookie-parser');
 
@@ -16,7 +18,7 @@ app.set('views', './views');
 
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(cookieParser('vuvietduc1234'));
+app.use(cookieParser(process.env.SESSION_SECRET));
 
 app.use(express.static('public'));
 
