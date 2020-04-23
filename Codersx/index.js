7c046d665c4b59c240162347f92f5505955cdd84
VuVietDuc <vuvietduc9x@gmail.com>
2020-04-23 16:39:55 +0700

    create route '/users'

--- Codersx/index.js
+++ Codersx/index.js
@@ -5,8 +5,12 @@ var fs = require('fs');
 var port = 3000;
 
 app.get('/', function (request, response) {
-	response.send("Hello CodersX!")
+	response.send('<h1>Hello CodersX!</h1><a href="/users">User List</a>')
 });
+
+app.get('/users', function (req, res) {
+	res.send('User list');
+})
 
 app.listen(port, function () {
 	console.log("Server listening on: " + port);
