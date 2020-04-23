aae178af5905b4521d323ce6c286cbe09e1f481d
VuVietDuc <vuvietduc9x@gmail.com>
2020-04-23 21:00:27 +0700

    query parameters

--- Codersx/index.js
+++ Codersx/index.js
@@ -7,17 +7,30 @@ var port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+var users = [
+			{id: 1, name: 'Đức'},
+			{id: 2, name: 'Thúy'}
+		];
+
 app.get('/', function (req, res) {
 	res.render('index', {name : 'Đức'});
 });
 
 app.get('/users', function (req, res) {
 	res.render('users/index', {
-		users: [
-			{id: 1, name: 'Đức'},
-			{id: 2, name: 'Thúy'}
-		]
+		users: users
 	});
+})
+
+app.get('/users/search', function (req, res) {
+	var q = req.query.q.toLowerCase();
+	var matchedUsers = users.filter(function (user) {
+		return user.name.toLowerCase().indexOf(q) > -1;
+	});
+
+	res.render('users/index', {
+		users: matchedUsers
+	})
 })
 
 app.listen(port, function () {
