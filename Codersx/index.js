207b0512b91139973f63f3d3264e9a900e9652e8
VuVietDuc <vuvietduc9x@gmail.com>
2020-04-24 10:18:13 +0700

    install module shortid and use

--- Codersx/index.js
+++ Codersx/index.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var low = require('lowdb');
 var FileSync = require('lowdb/adapters/FileSync');
+var shortid = require('shortid');
 
 var adapter = new FileSync('db.json')
 var db = low(adapter)
@@ -43,7 +44,15 @@ app.get('/users/create', function (req, res) {
 	res.render('users/create');
 })
 
+app.get('/users/:id', function (req, res) {
+	var id = req.params.id;
+	res.render('users/view', {
+		user: db.get('users').find({id: id}).value()
+	})
+})
+
 app.post('/users/create', function (req, res) {
+	req.body.id = shortid.generate();
 	db.get('users').push(req.body).write();
 	res.redirect('/users');
 })
